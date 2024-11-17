f02907951d6c52c96c62d41c5993b079f7f22e01
deswaq1220 <deswaq789@naver.com>
2024-11-17 23:18:37 +0900

    🚚 학생,선생 네브바 경로 이동

--- src/layouts/studentLayout.tsx
+++ src/layouts/studentLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router';
-import StudentNavBar from '../components/common/StudentNavBar';
+import StudentNavBar from '../components/naviatorBar/StudentNavBar';
 
 const StudentLayout = () => {
   return (
