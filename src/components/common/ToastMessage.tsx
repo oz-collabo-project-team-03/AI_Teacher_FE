44ea88f850e4f53a233b420f7843fc491b642568
deswaq1220 <deswaq789@naver.com>
2024-11-18 02:25:14 +0900

    ♻️ visibility 속성으로 변경

--- src/components/common/ToastMessage.tsx
+++ src/components/common/ToastMessage.tsx
@@ -2,15 +2,11 @@ import { useEffect, useState } from 'react';
 
 type ToastProps = {
   message: string;
-  duration?: number; // 토스트 메시지가 표시되는 시간 (ms)
+  duration?: number;
   onClose?: () => void;
 };
 
-const ToastMessage = ({
-  message,
-  duration = 3000, // 기본 3초
-  onClose,
-}: ToastProps) => {
+const ToastMessage = ({ message, duration = 3000, onClose }: ToastProps) => {
   const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
@@ -22,13 +18,15 @@ const ToastMessage = ({
     return () => clearTimeout(timer);
   }, [duration, onClose]);
 
-  if (!isVisible) return null;
   return (
-    <div className='animate-fade-in fixed bottom-32 left-1/2 z-50 -translate-x-1/2'>
+    <div
+      className={`fixed bottom-32 left-1/2 z-50 -translate-x-1/2 transition-[visibility,opacity] duration-200 ${isVisible ? 'visible opacity-100' : 'invisible opacity-0'}`}
+    >
       <div className='rounded bg-commuBgColor px-[14px] py-1 text-sm text-textMainColor'>
         {message}
       </div>
     </div>
   );
 };
+
 export default ToastMessage;
