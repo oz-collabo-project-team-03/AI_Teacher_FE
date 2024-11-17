724a1dc20fc3427d70fea4b1b65792c5bc80c582
deswaq1220 <deswaq789@naver.com>
2024-11-17 23:33:36 +0900

    🔥 preventBack 코드 삭제

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,21 +4,18 @@ import { backIcon } from '../../assets/assets';
 type HeaderProps = {
   title: string;
   rightElement?: React.ReactNode;
-  preventBack?: boolean; // 특별한 경우 뒤로가기를 막아야 할 때 사용
 };
 
-const Header = ({ title, rightElement, preventBack = false }: HeaderProps) => {
+const Header = ({ title, rightElement }: HeaderProps) => {
   const navigate = useNavigate();
 
-  const handleBack = () => {
-    if (!preventBack) {
-      navigate(-1);
-    }
-  };
+  // const handleBack = () => {
+  //   navigate(-1);
+  // };
   return (
     <header className='flex items-center justify-between border-b border-borderColor bg-white px-2 py-6'>
       <div className='flex items-center'>
-        <button onClick={handleBack} className='mr-2'>
+        <button onClick={() => navigate(-1)} className='mr-2'>
           <img src={backIcon} alt='' />
         </button>
 
