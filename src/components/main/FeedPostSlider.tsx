70560b4c0c84d0f3dbcb76d8fd13d1fb88dec69e
doin-N <tlatms777@gmail.com>
2024-11-19 05:35:50 +0900

    💄 Style: 피드부분 스타일 수정

--- src/components/main/FeedPostSlider.tsx
+++ src/components/main/FeedPostSlider.tsx
@@ -1,6 +1,6 @@
 const FeedPostSlider = () => {
   return (
-    <div className='h-[192px] w-full [background-color:#CB9DF0]'>
+    <div className='h-[233px] w-full [background-color:#CB9DF0]'>
       <div>MainSlider</div>
     </div>
   );
--- src/pages/main/homeFeedPage.tsx
+++ src/pages/main/homeFeedPage.tsx
@@ -2,10 +2,16 @@ import FeedPost from '../../components/main/FeedPost';
 import MainHeader from '../../components/common/MainHeader';
 
 const HomeFeedPage = () => {
+  const posts = [1, 2, 3]; //임시배열
+
   return (
     <div>
       <MainHeader />
-      <FeedPost />
+      <div>
+        {posts.map((index) => (
+          <FeedPost key={index} />
+        ))}
+      </div>
     </div>
   );
 };
