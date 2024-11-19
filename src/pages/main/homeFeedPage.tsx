870c243e295e35453e0472b12c3d67776c2c7210
doin-N <tlatms777@gmail.com>
2024-11-19 13:38:12 +0900

    STYLE 💄:  학생 메인 페이지 스타일 수정

--- src/pages/main/homeFeedPage.tsx
+++ src/pages/main/homeFeedPage.tsx
@@ -6,8 +6,13 @@ const HomeFeedPage = () => {
 
   return (
     <div>
-      <MainHeader />
-      <div>
+      <div className='fixed left-1/2 top-0 w-full -translate-x-1/2 transform bg-white md:w-[425px] lg:w-[425px]'>
+        <MainHeader />
+      </div>
+      <div
+        className='scrollbar-none overflow-y-auto pb-[62px] pt-[72px]'
+        style={{ height: 'calc(100vh - 72px)' }}
+      >
         {posts.map((index) => (
           <FeedPost key={index} />
         ))}
