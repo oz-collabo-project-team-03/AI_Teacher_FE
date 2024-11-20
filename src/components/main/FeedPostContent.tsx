2bb9c03d8d995691d1dcc9150f29ea7f5bb39aa0
doin-N <tlatms777@gmail.com>
2024-11-21 04:59:43 +0900

    🎨 Design: FeedPostContent 컴포넌트 폰트 크기 및 여백 스타일 조정

--- src/components/main/FeedPostContent.tsx
+++ src/components/main/FeedPostContent.tsx
@@ -38,25 +38,25 @@ const FeedPostContent = () => {
       </li>
 
       {/* 협업멘트 */}
-      <li className='mt-[6px] flex h-[20px] w-full items-center text-[10px]'>
+      <li className='mt-[6px] flex h-[20px] w-full items-center text-[14px]'>
         <img
           src={teacher}
           alt='teacherProfileImage'
-          className='mr-[3px] h-[20px] w-[20px]'
+          className='mr-[5px] h-[20px] w-[20px]'
         />
         <span>도인핑 선생님과 협업하였습니다.</span>
       </li>
 
       {/* 댓글축약 */}
-      <li className='my-[7px] flex h-[12px] w-full'>
-        <p className='mr-[6px] text-[10px]'>
+      <li className='my-[7px] flex h-[14px] w-full'>
+        <p className='mr-[6px] text-[14px]'>
           이번에 수학 수업 수행평가 해봤습니다 ㅎㅎ ...
         </p>
-        <button className='text-[10px] text-captionColor'>더보기</button>
+        <button className='text-[14px] text-captionColor'>더보기</button>
       </li>
       <time
         dateTime='2024-10-15'
-        className='block w-full text-[10px] text-captionColor'
+        className='block w-full text-[14px] text-captionColor'
       >
         24년 10월 15일
       </time>
