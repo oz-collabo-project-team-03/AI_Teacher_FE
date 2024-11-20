8558084c5432cd06ed261dd6319248eb421d3576
deswaq1220 <deswaq789@naver.com>
2024-11-21 01:33:00 +0900

    ♻️ ref 속성 추가

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 type InputProps = React.ComponentPropsWithoutRef<'input'>;
 
-const Input = (props: InputProps) => {
+const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   return (
     <input
+      ref={ref}
       className='w-full rounded-[10px] border-0 px-5 py-[18px] outline-none ring-1 ring-inset ring-inputBorderColor placeholder:text-inputBorderColor focus:ring-2 focus:ring-inset focus:ring-inputFocusColor'
       {...props}
     />
   );
-};
+});
 
 export default Input;
