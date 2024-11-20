c170d2f841b0068d593d55642d9202748fe338ae
doin-N <tlatms777@gmail.com>
2024-11-21 04:58:51 +0900

    ♻️ Refactor: MainHeader 컴포넌트에 className prop 추가

--- src/components/common/MainHeader.tsx
+++ src/components/common/MainHeader.tsx
@@ -1,6 +1,12 @@
-const MainHeader = () => {
+type MainHeaderProps = {
+  className?: string;
+};
+
+const MainHeader = ({ className }: MainHeaderProps) => {
   return (
-    <header className='fixed top-0 flex h-[72px] w-full items-center justify-center border-b border-borderColor bg-white md:w-[425px]'>
+    <header
+      className={`fixed top-0 flex h-[72px] w-full items-center justify-center border-b border-borderColor bg-white md:w-[425px] ${className}`}
+    >
       <h1 className='font-gMarket text-[30px]'>수행쌤</h1>
     </header>
   );
