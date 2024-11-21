37c654161f491eda7c6014127b68e8a2928e2533
Watnu03 <watnu03@gmail.com>
2024-11-21 13:21:41 +0900

    ♻️ Rafactor: REact.FC 삭제후 수정,border 하나로 통일

--- src/components/chat/bubbles/ChatAiBubble.tsx
+++ src/components/chat/bubbles/ChatAiBubble.tsx
@@ -1,11 +1,10 @@
 import { ChatBubbleProps } from '../../../types/index';
-import React from 'react';
 
-const ChatAiBubble: React.FC<ChatBubbleProps> = ({ message }) => {
+const ChatAiBubble = ({ message }: ChatBubbleProps) => {
   return (
     <div className='flex'>
       {/* 말풍선 꼬리 */}
-      <div className='h-0 w-0 border-b-[10px] border-l-[10px] border-r-[10px] border-t-[10px] border-b-transparent border-l-transparent border-r-transparent border-t-chatBubbleColor'></div>
+      <div className='h-0 w-0 border-[10px] border-b-transparent border-l-transparent border-r-transparent border-t-chatBubbleColor'></div>
       <div className='relative left-[-15px] max-w-[245px] rounded-[10px] bg-chatBubbleColor p-[14px]'>
         <p className='break-words text-[14px] font-normal text-textMainColor'>
           {message}
