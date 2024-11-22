6a879e3e8922f54d9a9010300d048ee346f49ec8
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-22 22:45:50 +0900

    ✨ mock api 연결 및 스토어 사용
    #35

--- src/components/editProfile/ProfileImages.tsx
+++ src/components/editProfile/ProfileImages.tsx
@@ -14,35 +14,47 @@ import studentIcon12 from '../../assets/editProfile/student/studentIcon12.png';
 import teacherDefaultIcon from '../../assets/editProfile/teacher/teacherDefaultIcon.png';
 import teacherIcon2 from '../../assets/editProfile/teacher/teacherIcon2.png';
 import teacherIcon3 from '../../assets/editProfile/teacher/teacherIcon3.png';
+import { useEffect } from 'react';
 
 type ProfileImagesProps = {
   selectedIndex: number;
-  onImageSelect: (index: number) => void;
+  onImageSelect: (index: number, imageUrl: string) => void;
   userType: 'student' | 'teacher';
+  currentImageUrl: string;
+};
+
+export const profileImages = {
+  student: [
+    studentDefaultIcon,
+    studentIcon2,
+    studentIcon3,
+    studentIcon4,
+    studentIcon5,
+    studentIcon6,
+    studentIcon7,
+    studentIcon8,
+    studentIcon9,
+    studentIcon10,
+    studentIcon11,
+    studentIcon12,
+  ],
+  teacher: [teacherDefaultIcon, teacherIcon2, teacherIcon3],
 };
 
 const ProfileImages = ({
   selectedIndex,
   onImageSelect,
   userType,
+  currentImageUrl,
 }: ProfileImagesProps) => {
-  const profileImages = {
-    student: [
-      studentDefaultIcon,
-      studentIcon2,
-      studentIcon3,
-      studentIcon4,
-      studentIcon5,
-      studentIcon6,
-      studentIcon7,
-      studentIcon8,
-      studentIcon9,
-      studentIcon10,
-      studentIcon11,
-      studentIcon12,
-    ],
-    teacher: [teacherDefaultIcon, teacherIcon2, teacherIcon3],
-  };
+  useEffect(() => {
+    const currentIndex = profileImages[userType].findIndex(
+      (img) => img === currentImageUrl
+    );
+    if (currentIndex !== -1) {
+      onImageSelect(currentIndex, currentImageUrl);
+    }
+  }, []);
 
   return (
     <div className='flex max-w-[410px] flex-wrap justify-between gap-x-4 gap-y-5 self-center pb-9'>
@@ -58,7 +70,7 @@ const ProfileImages = ({
             src={img}
             alt={`${userType} 프로필 이미지${index + 1}`}
             className='h-[60px] w-[60px] rounded-full object-cover'
-            onClick={() => onImageSelect(index)}
+            onClick={() => onImageSelect(index, img)}
           />
         </div>
       ))}
--- src/pages/myPage/myPage.tsx
+++ src/pages/myPage/myPage.tsx
@@ -1,12 +1,75 @@
-import StudentPage from '../../components/myPage/StudentPage';
-// import TeacherPage from '../../components/myPage/TeacherPage';
+import { useEffect, useState } from 'react';
+import {
+  StudentMyPageResponse,
+  TeacherMyPageResponse,
+} from '../../types/myPageType.ts';
+import StudentPage from '../../components/myPage/StudentPage.tsx';
+import TeacherPage from '../../components/myPage/TeacherPage.tsx';
+import { useProfileStore } from '../../stores/editProfile/useProfileStore.ts';
 
 const MyPage = () => {
+  const { userInfo, setUserInfo } = useProfileStore();
+  const [communityInfo, setCommunityInfo] = useState<
+    { label: string; value: number }[]
+  >([]);
+
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      if (!userInfo) {
+        try {
+          const response = await fetch('/api/profile/me');
+          const result = await response.json();
+
+          if (result.success) {
+            setUserInfo(result.data);
+          }
+        } catch (error) {
+          console.error(
+            '프로필 데이터를 불러오는 중 오류가 발생했습니다:',
+            error
+          );
+        }
+      }
+    };
+
+    fetchProfileData();
+  }, [userInfo, setUserInfo]);
+
+  useEffect(() => {
+    if (userInfo) {
+      setCommunityInfo(
+        userInfo.role === 'student'
+          ? [
+              { label: '게시글', value: userInfo.post_count },
+              { label: '좋아요', value: userInfo.like_count },
+              { label: '작성 댓글', value: userInfo.comment_count },
+            ]
+          : [
+              { label: '협업 게시글', value: userInfo.post_count },
+              { label: '좋아요', value: userInfo.like_count },
+              { label: '작성 댓글', value: userInfo.comment_count },
+            ]
+      );
+    }
+  }, [userInfo]);
+
+  if (!userInfo) {
+    return null;
+  }
+
   return (
-    <div className='flex w-full flex-col items-center justify-center overflow-y-scroll pb-[62px]'>
-      {/* 로그인한 유저의 역할에 따라 컴포넌트 변경 */}
-      <StudentPage />
-      {/* <TeacherPage /> */}
+    <div className='m-auto flex w-full max-w-[360px] flex-col items-center gap-9 py-12'>
+      {userInfo.role === 'student' ? (
+        <StudentPage
+          userInfo={userInfo as StudentMyPageResponse}
+          communityInfo={communityInfo}
+        />
+      ) : (
+        <TeacherPage
+          userInfo={userInfo as TeacherMyPageResponse}
+          communityInfo={communityInfo}
+        />
+      )}
     </div>
   );
 };
