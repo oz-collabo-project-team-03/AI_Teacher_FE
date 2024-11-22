abc8e8040dda65c3504558f8f60a3ac6d5ff09a9
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-22 22:43:00 +0900

    ✨ 유저 프로필 스토어
    #35

---
+++ src/stores/editProfile/useProfileStore.ts
@@ -0,0 +1,31 @@
+import { create } from 'zustand';
+import { MyPageResponseData } from '../../types/myPageType';
+import { EditProfileRequestData } from '../../types/editProfile';
+
+type ProfileStore = {
+  userInfo: MyPageResponseData | null;
+  setUserInfo: (info: MyPageResponseData) => void;
+  updateProfile: (data: Partial<EditProfileRequestData>) => void;
+};
+
+export const useProfileStore = create<ProfileStore>((set) => ({
+  userInfo: null,
+  setUserInfo: (info) => set({ userInfo: info }),
+  updateProfile: (data) =>
+    set((state) => {
+      if (!state.userInfo) return state;
+
+      const updatedInfo = {
+        ...state.userInfo,
+        ...data,
+        post_count: state.userInfo.post_count,
+        like_count: state.userInfo.like_count,
+        comment_count: state.userInfo.comment_count,
+        posts: state.userInfo.posts,
+      };
+
+      return {
+        userInfo: updatedInfo as MyPageResponseData,
+      };
+    }),
+}));
