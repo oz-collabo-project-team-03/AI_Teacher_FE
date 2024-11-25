ec776fd501f9763ae371ae5b948260cd74690385
doin-N <tlatms777@gmail.com>
2024-11-25 14:59:00 +0900

    💄 Style: 스타일 수정

--- src/pages/post/createPostPage.tsx
+++ src/pages/post/createPostPage.tsx
@@ -3,15 +3,8 @@ import CheckBox from '../../components/common/CheckBox';
 import Header from '../../components/common/Header';
 import PostImageUpload from '../../components/post/PostImageUpload';
 import PostTextEditor from '../../components/post/PostTextEditor';
-import { useState } from 'react';
 
 const CreatePostPage = () => {
-  const [selectedButton, setSelectedButton] = useState<string | null>(null);
-
-  const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setSelectedButton(e.currentTarget.name);
-  };
-
   return (
     <main className='space-y-3'>
       <Header title='포스팅하기' />
@@ -28,17 +21,15 @@ const CreatePostPage = () => {
 
       <footer className='flex h-[62px] items-center justify-center gap-2.5 text-[18px]'>
         <Button
-          onClick={handleButtonClick}
+          // onClick={handleButtonClick}
           name='cancel'
-          variant={selectedButton === 'cancel' ? 'active' : 'cancel'}
-          className='w-[196.5px]'
+          className='w-[196.5px] bg-cancelButtonColor hover:bg-hobbyText'
         >
           취소
         </Button>
         <Button
-          onClick={handleButtonClick}
+          // onClick={handleButtonClick}
           name='post'
-          variant={selectedButton === 'post' ? 'active' : 'cancel'}
           className='w-[196.5px]'
         >
           포스팅
