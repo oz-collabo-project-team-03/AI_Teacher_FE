16a45696f876a1ecd3a5fd19cf3fb250296902d6
doin-N <tlatms777@gmail.com>
2024-11-25 15:40:01 +0900

    🐛 Fix: 특정 페이지 수정 중 전역 Input에 잘못 반영된 변경 사항 복구

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -6,7 +6,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   return (
     <input
       ref={ref}
-      className='w-full rounded-[10px] border-0 px-5 py-[18px] outline-none ring-1 ring-inset ring-inputBorderColor placeholder:text-inputBorderColor focus:ring-0 focus:ring-inset'
+      className='w-full rounded-[10px] border-0 px-5 py-[18px] outline-none ring-1 ring-inset ring-inputBorderColor placeholder:text-inputBorderColor focus:ring-2 focus:ring-inset focus:ring-inputFocusColor'
       {...props}
     />
   );
