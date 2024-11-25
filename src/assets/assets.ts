69a65b47a0156c51297e286fb0418c132c5b0df7
Watnu03 <watnu03@gmail.com>
2024-11-25 13:30:58 +0900

    🐛 Fix: git 출동 에러 해결

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -8,6 +8,7 @@ import googleLogo from './auth/google_login.svg';
 import homeActiveIcon from './navBar/home_active.svg';
 import iphone from './landing/iphone.svg';
 import kakaoLogo from './auth/kakao_logo.png';
+import logoutIcon from './myPage/logoutIcon.svg';
 import modalCloseIcon from './modal/modal_close.svg';
 import naverLogo from './auth/naver_logo.svg';
 import plusActiveIcon from './navBar/student/plus_active.svg';
@@ -16,13 +17,16 @@ import roleStudentUnActive from './roleSelect/role_student_unactive.svg';
 import roleTeacherActive from './roleSelect/role_teacher_active.svg';
 import roleTeacherUnActive from './roleSelect/role_teacher_unactive.svg';
 import thumbsUPIcon from './auth/thumbs_up.svg';
+import toggleIcon from './myPage/toggleIcon.svg';
 import userActiveIcon from './navBar/teacher/user_Active.svg';
 
 export {
   backIcon,
   chatActiveIcon,
   chatBubbleIcon,
+  toggleIcon,
   editIcon,
+  logoutIcon,
   googleLogo,
   homeActiveIcon,
   kakaoLogo,
