1a76d82e9fa9dceaaadede3528cfee8e981973fa
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-25 18:47:32 +0900

    ♻️ 누락 핸들러 임포트
    #64

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,7 +1,12 @@
 import { setupWorker } from 'msw/browser';
 import { handlers } from './handlers';
 import { myPageHandlers } from './myPageHandlers';
+import { editProfileHandlers } from './editProfileHandlers';
 
-export const worker = setupWorker(...handlers, ...myPageHandlers);
+export const worker = setupWorker(
+  ...handlers,
+  ...myPageHandlers,
+  ...editProfileHandlers
+);
 
 worker.start();
