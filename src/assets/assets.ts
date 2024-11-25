b474249013ea865e70b23ac7d74eadfbd3f7a1a1
deswaq1220 <deswaq789@naver.com>
2024-11-25 15:56:30 +0900

    🔥 회원쪽 아이콘 이미지 import 코드 삭제

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,45 +1,30 @@
-import backIcon from './header/back.svg';
-import chatActiveIcon from './navBar/chat_active.svg';
-import chatBubbleIcon from './header/chat.svg';
 import chatDeleteIcon from './chat/chat_delete.svg';
 import chatPlusIcon from './chat/chat_plus.svg';
-import toggleIcon from './myPage/toggleIcon.svg';
+import backIcon from './header/back.svg';
+import chatBubbleIcon from './header/chat.svg';
+import iphone from './landing/iphone.svg';
+import modalCloseIcon from './modal/modal_close.svg';
 import editIcon from './myPage/editIcon.svg';
 import logoutIcon from './myPage/logoutIcon.svg';
-import googleLogo from './auth/google_login.svg';
+import toggleIcon from './myPage/toggleIcon.svg';
+import chatActiveIcon from './navBar/chat_active.svg';
 import homeActiveIcon from './navBar/home_active.svg';
-import iphone from './landing/iphone.svg';
-import kakaoLogo from './auth/kakao_logo.png';
-import modalCloseIcon from './modal/modal_close.svg';
-import naverLogo from './auth/naver_logo.svg';
 import plusActiveIcon from './navBar/student/plus_active.svg';
-import roleStudentActive from './roleSelect/role_student_active.svg';
-import roleStudentUnActive from './roleSelect/role_student_unactive.svg';
-import roleTeacherActive from './roleSelect/role_teacher_active.svg';
-import roleTeacherUnActive from './roleSelect/role_teacher_unactive.svg';
-import thumbsUPIcon from './auth/thumbs_up.svg';
+
 import userActiveIcon from './navBar/teacher/user_Active.svg';
 
 export {
   backIcon,
   chatActiveIcon,
   chatBubbleIcon,
-  toggleIcon,
-  editIcon,
-  logoutIcon,
-  googleLogo,
-  homeActiveIcon,
-  kakaoLogo,
-  naverLogo,
-  plusActiveIcon,
-  roleStudentActive,
-  roleStudentUnActive,
-  roleTeacherActive,
-  roleTeacherUnActive,
-  thumbsUPIcon,
-  userActiveIcon,
-  iphone,
   chatDeleteIcon,
   chatPlusIcon,
+  editIcon,
+  homeActiveIcon,
+  iphone,
+  logoutIcon,
   modalCloseIcon,
+  plusActiveIcon,
+  toggleIcon,
+  userActiveIcon,
 };
