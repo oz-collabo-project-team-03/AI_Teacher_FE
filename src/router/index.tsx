1a9c36c38aec5a1f701cb1444823342c1b44efc1
deswaq1220 <deswaq789@naver.com>
2024-11-26 19:19:09 +0900

    ✅ 라우터 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,8 +5,10 @@ import DefaultLayout from '../layouts/defaultLayout';
 import StudentLayout from '../layouts/studentLayout';
 import TeacherLayout from '../layouts/teacherLayout';
 
+import ErrorPage from '@/pages/status/errorPage';
+import NotfoundPage from '@/pages/status/notfoundPage';
 import Landing from '../pages/landing/landing';
-import LoadingPage from '../pages/loadingPage';
+import LoadingPage from '../pages/status/loadingPage';
 
 const HomeFeedPage = lazy(() => import('../pages/main/homeFeedPage'));
 const ManagedStudentListPage = lazy(
@@ -50,8 +52,9 @@ const Router = () => {
     <>
       <Routes>
         <Route element={<DefaultLayout />}>
-          <Route path='*' />
+          <Route path='*' element={<NotfoundPage />} />
           <Route path='/' element={<Landing />} />
+          <Route path='/error' element={<ErrorPage />} />
           <Route
             path='/login'
             element={
