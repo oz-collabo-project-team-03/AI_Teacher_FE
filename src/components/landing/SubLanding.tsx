651819de3d69e71b63628fbd763dfc4e80fb87df
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-27 18:18:10 +0900

    🎨 서브 랜딩페이지 애니메이션 추가
    #73

--- src/components/landing/SubLanding.tsx
+++ src/components/landing/SubLanding.tsx
@@ -1,24 +1,41 @@
 import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion'; // framer-motion import 추가
-import { iphone } from '@/assets/assets';
+import { motion } from 'framer-motion';
+import { iphone, bottomArrow } from '@/assets/assets';
 
 const SubLanding = () => {
+  const phoneVariants = {
+    animate: {
+      scale: [1.03, 1.07, 1.03],
+      transition: {
+        duration: 2,
+        repeat: Infinity,
+        repeatType: 'loop' as const,
+        ease: 'easeInOut',
+        times: [0, 0.5, 1],
+      },
+    },
+  };
+
   return (
-    <div className='flex flex-col items-center gap-[50px] pt-[200px]'>
+    <div className='flex flex-col items-center gap-[20px] pt-[150px]'>
       <Link to='/login'>
-        <span className='text-3xl font-bold text-mainLogoTextColor'>
-          수행쌤 만나러 가기!
+        <span className='text-2xl font-bold text-mainLogoTextColor'>
+          나만의 수행쌤 만나러 가기!
         </span>
       </Link>
 
-      <motion.img
-        src={iphone}
-        alt='아이폰 이미지'
-        draggable='false'
-        className='w-[324px] select-none'
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.95 }}
-      />
+      <img src={bottomArrow} alt='아래 화살표' className='pb-[25px]' />
+
+      <Link to='/login'>
+        <motion.img
+          src={iphone}
+          alt='아이폰 이미지'
+          draggable='false'
+          className='w-[324px] select-none'
+          animate='animate'
+          variants={phoneVariants}
+        />
+      </Link>
     </div>
   );
 };
