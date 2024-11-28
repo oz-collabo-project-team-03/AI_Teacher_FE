645daf3a4bbaa445f55b78242ca71e81e5a31611
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-28 19:31:40 +0900

    ✨ 무한스크롤 훅 생성
    #79

---
+++ src/hooks/useInfiniteScroll.ts
@@ -0,0 +1,38 @@
+// hooks/useInfiniteScroll.ts
+import { useEffect, useRef, useCallback } from 'react';
+
+interface UseInfiniteScrollProps {
+  fetchNextPage: () => void;
+  hasNextPage: boolean | undefined;
+}
+
+export const useInfiniteScroll = ({
+  fetchNextPage,
+  hasNextPage,
+}: UseInfiniteScrollProps) => {
+  const observerRef = useRef<HTMLDivElement>(null);
+
+  const handleObserver = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      const [target] = entries;
+      if (target.isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    },
+    [fetchNextPage, hasNextPage]
+  );
+
+  useEffect(() => {
+    const element = observerRef.current;
+    const option = { threshold: 0.5 };
+
+    const observer = new IntersectionObserver(handleObserver, option);
+    if (element) observer.observe(element);
+
+    return () => {
+      if (element) observer.unobserve(element);
+    };
+  }, [handleObserver]);
+
+  return observerRef;
+};
