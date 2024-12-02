6d799e7400eb9ef5d7ca3786fa4f1b0393363a21
doin-N <tlatms777@gmail.com>
2024-12-02 12:41:32 +0900

    ✨ Feat: 이미지 업로드 기능 개선 및 React Hook Form 통합
    #90

--- src/components/posting/PostImageUpload.tsx
+++ src/components/posting/PostImageUpload.tsx
@@ -1,36 +1,34 @@
-import { useRef, useState } from 'react';
-
 import deleteIcon from '../../assets/posting/deleteIcon.svg';
 import photo from '../../assets/posting/photo.svg';
+import { useFormContext } from 'react-hook-form';
+import { useState } from 'react';
+import { useToast } from '@/hooks/useToast';
 
-function PostImageUpload() {
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const [images, setImages] = useState<string[]>([]);
+const PostImageUpload = () => {
+  const { setValue, getValues } = useFormContext();
+  const [images, setImages] = useState<string[]>(getValues('images') || []);
+  const { showToast } = useToast();
 
-  //버튼하고 input연결
-  const handleButtonClick = () => {
-    if (images.length < 3) {
-      fileInputRef.current?.click();
+  const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (!files) return;
+
+    const fileArray = Array.from(files);
+    const newImages = fileArray.map((file) => URL.createObjectURL(file));
+
+    if (images.length + newImages.length > 3) {
+      showToast('사진은 최대 3개까지 업로드 가능합니다.');
+      return;
     }
+    const updatedImages = [...images, ...newImages];
+    setImages(updatedImages);
+    setValue('images', updatedImages);
   };
 
-  //프리뷰용 Data URL 형식변환 (Base64) 방식추후상의필요
-  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target?.files?.[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        if (images.length < 3) {
-          setImages((prevImages) => [...prevImages, reader.result as string]);
-        }
-      };
-      reader.readAsDataURL(file);
-    }
-  };
-
-  // 이미지 삭제
   const handleDeleteImage = (index: number) => {
-    setImages((prevImages) => prevImages.filter((_, i) => i !== index));
+    const updatedImages = images.filter((_, i) => i !== index);
+    setImages(updatedImages);
+    setValue('images', updatedImages);
   };
 
   return (
@@ -38,53 +36,48 @@ function PostImageUpload() {
       <p className='text-[14px] text-captionColor'>
         사진 파일만 선택해 주세요. (지원 형식: JPG, JPEG, PNG)
       </p>
-      <div className='flex gap-2.5'>
-        <button
-          className='flex h-[90px] w-[90px] cursor-pointer flex-col items-center justify-center rounded-[10px] border border-postTextBorderColor'
-          onClick={handleButtonClick}
-        >
+      <div className='flex gap-2'>
+        <label className='relative flex h-[92px] w-[92px] flex-col items-center justify-center rounded-md border'>
           <img
             src={photo}
-            alt='default-photo-icon'
-            className='object-cover object-center' // 기본 사진 아이콘
+            alt='upload-placeholder'
+            className='h-[30px] w-[30px]'
           />
           <p className='right-[5px] top-[5px] mt-[5px] text-[12px] font-bold text-hobbyText'>
             <span className='text-profilePointTextColor'>{images.length}</span>
             /3
           </p>
-        </button>
+          <input
+            type='file'
+            className='hidden'
+            accept='image/jpeg, image/png, image/jpg'
+            multiple
+            onChange={handleFileSelect}
+          />
+        </label>
+
         {images.map((image, index) => (
           <div
-            key={image}
-            className='relative box-border flex h-[90px] w-[90px] items-center justify-center overflow-hidden rounded-[10px] border border-postTextBorderColor'
+            key={index}
+            className='relative flex h-[92px] w-[92px] items-center justify-center rounded-md border'
           >
             <img
               src={image}
-              alt={`photo icon ${index}`}
-              className='object-cover object-center'
+              alt='uploaded-thumbnail'
+              className='h-full w-full rounded-md object-cover'
             />
             <button
+              type='button'
+              className='absolute right-1 top-1'
               onClick={() => handleDeleteImage(index)}
-              className='absolute right-[5px] top-[5px]'
             >
-              <img
-                src={deleteIcon}
-                alt='delete-icon'
-                className='h-[17px] w-[17px]'
-              />
+              <img src={deleteIcon} alt='delete-icon' className='h-4 w-4' />
             </button>
           </div>
         ))}
       </div>
-      <input
-        ref={fileInputRef}
-        type='file'
-        className='hidden'
-        accept='image/jpeg, image/png, image/jpg'
-        onChange={handleImageUpload}
-      />
     </div>
   );
-}
+};
 
 export default PostImageUpload;
