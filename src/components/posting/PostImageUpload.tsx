a3f130fd79f554a96f9b9c1a6d67ff9639b50d35
doin-N <tlatms777@gmail.com>
2024-12-02 15:18:29 +0900

    🎨 Design: 버튼 크기 조정 및 레이아웃 수정

--- src/components/posting/PostImageUpload.tsx
+++ src/components/posting/PostImageUpload.tsx
@@ -32,7 +32,7 @@ const PostImageUpload = () => {
   };
 
   return (
-    <div className='my-[12px] flex h-[151px] w-full flex-col gap-1.5 px-[16px] py-[19px]'>
+    <div className='my-[12px] flex h-[151px] w-full flex-col gap-2 px-[16px] py-[19px]'>
       <p className='text-[14px] text-captionColor'>
         사진 파일만 선택해 주세요. (지원 형식: JPG, JPEG, PNG)
       </p>
