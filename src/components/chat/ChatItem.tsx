a792a5ab2527c56602316171bca575c4c7c36386
Watnu03 <watnu03@gmail.com>
2024-12-03 18:10:22 +0900

    ✨ Feat: 최신채팅시간 날짜별 표기법 추가

--- src/components/chat/ChatItem.tsx
+++ src/components/chat/ChatItem.tsx
@@ -1,9 +1,21 @@
+import { format, isToday, parse } from 'date-fns';
+
 type ChatItemProps = {
   roomName: string;
   lastMessage: string;
   lastMessageTime: string;
   showHelpRequest: boolean;
   onClick?: () => void;
+};
+
+const formatLastMessageTime = (lastMessageTime: string): string => {
+  const parsedDate = parse(lastMessageTime, 'yyyy-MM-dd HH시mm분', new Date());
+
+  if (isToday(parsedDate)) {
+    return format(parsedDate, 'HH시mm분');
+  } else {
+    return format(parsedDate, 'yyyy-MM-dd');
+  }
 };
 
 const ChatItem = ({
@@ -18,7 +30,9 @@ const ChatItem = ({
     onClick={onClick}
   >
     <div className='flex'>
-      <div className='text-[16px] font-medium'>{roomName}</div>
+      <div className='max-w-[calc(100%-65px)] overflow-hidden text-ellipsis whitespace-nowrap text-[16px] font-medium'>
+        {roomName}
+      </div>
       {showHelpRequest && (
         <div className='ml-2'>
           <span className='h-[22px] w-[57px] rounded-[4px] bg-helpButtonColor p-[6px] text-[14px] font-bold text-white'>
@@ -28,8 +42,12 @@ const ChatItem = ({
       )}
     </div>
     <div className='mt-1 flex justify-between'>
-      <div className='text-[14px] text-chatText'>{lastMessage}</div>
-      <div className='text-[14px] text-captionColor'>{lastMessageTime}</div>
+      <div className='max-w-[65%] overflow-hidden text-ellipsis whitespace-nowrap text-[14px] text-chatText'>
+        {lastMessage}
+      </div>
+      <div className='text-[14px] text-captionColor'>
+        {formatLastMessageTime(lastMessageTime)}
+      </div>
     </div>
   </div>
 );
