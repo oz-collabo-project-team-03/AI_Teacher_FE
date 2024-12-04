39e9354907834b55e5d07a0024f8b2c93d01bf45
Yi-HyeonJu <guswwn1925@gmail.com>
2024-12-04 22:19:34 +0900

    🎨 탈퇴버튼 생성
    #108

--- src/components/changeProfile/EditAccount.tsx
+++ src/components/changeProfile/EditAccount.tsx
@@ -19,86 +19,95 @@ const EditAccount = () => {
   } = useEditAccountForm(userInfo?.role);
 
   return (
-    <FormProvider {...form}>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className='flex h-full flex-col justify-between px-4 pb-[30px] pt-[38px]'
-      >
-        <div className='flex h-full flex-col gap-4'>
-          <div>
-            <AuthInput
-              type='password'
-              placeholder='패스워드를 입력해주세요.'
-              label='패스워드'
-              {...register('password')}
-            />
-            {errors.password && (
-              <p className='mt-1 whitespace-pre text-sm text-errorTextColor'>
-                {errors.password.message}
-              </p>
-            )}
-            {!errors.password && (
-              <p className='mt-1 whitespace-pre text-xs text-captionColor'>
-                문자, 숫자, 특수문자(!@#$%^&*)포함 10~20자리 이내
-              </p>
-            )}
-          </div>
-          <div>
-            <AuthInput
-              type='password'
-              placeholder='패스워드를 재입력해주세요.'
-              label='패스워드 확인'
-              {...register('confirmPassword')}
-            />
-            {errors.confirmPassword && (
-              <p className='mt-1 text-sm text-errorTextColor'>
-                {errors.confirmPassword.message}
-              </p>
-            )}
-          </div>
-
-          <div>
-            <AuthInput
-              type='number'
-              placeholder='-없이 입력해주세요.'
-              label='연락처'
-              {...register('phone')}
-            />
-            {errors.phone && (
-              <p className='mt-1 text-sm text-errorTextColor'>
-                {errors.phone.message}
-              </p>
-            )}
-          </div>
-
-          {userInfo?.role === 'student' && (
-            <>
+    <div className='flex h-full flex-col gap-1 px-4 pb-[30px] pt-[38px]'>
+      <FormProvider {...form}>
+        <form onSubmit={handleSubmit(onSubmit)} className='h-full'>
+          <div className='flex h-full flex-col justify-between'>
+            <div className='flex flex-col gap-4'>
               <div>
                 <AuthInput
-                  type='text'
-                  placeholder='학교 이름을 입력해주세요.'
-                  label='학교'
-                  {...register('school')}
+                  type='password'
+                  placeholder='패스워드를 입력해주세요.'
+                  label='패스워드'
+                  {...register('password')}
                 />
-                {errors.school && (
-                  <p className='mt-1 text-sm text-errorTextColor'>
-                    {errors.school.message}
+                {errors.password && (
+                  <p className='mt-1 whitespace-pre text-sm text-errorTextColor'>
+                    {errors.password.message}
+                  </p>
+                )}
+                {!errors.password && (
+                  <p className='mt-1 whitespace-pre text-xs text-captionColor'>
+                    문자, 숫자, 특수문자(!@#$%^&*)포함 10~20자리 이내
                   </p>
                 )}
               </div>
-              <GradeSelector
-                selectedGrade={selectedGrade}
-                setSelectedGrade={setSelectedGrade}
-              />
-            </>
-          )}
-        </div>
 
-        <Button type='submit' variant='active'>
-          변경 내용 저장
-        </Button>
-      </form>
-    </FormProvider>
+              <div>
+                <AuthInput
+                  type='password'
+                  placeholder='패스워드를 재입력해주세요.'
+                  label='패스워드 확인'
+                  {...register('confirmPassword')}
+                />
+                {errors.confirmPassword && (
+                  <p className='mt-1 text-sm text-errorTextColor'>
+                    {errors.confirmPassword.message}
+                  </p>
+                )}
+              </div>
+
+              <div>
+                <AuthInput
+                  type='number'
+                  placeholder='-없이 입력해주세요.'
+                  label='연락처'
+                  {...register('phone')}
+                />
+                {errors.phone && (
+                  <p className='mt-1 text-sm text-errorTextColor'>
+                    {errors.phone.message}
+                  </p>
+                )}
+              </div>
+
+              {userInfo?.role === 'student' && (
+                <>
+                  <div>
+                    <AuthInput
+                      type='text'
+                      placeholder='학교 이름을 입력해주세요.'
+                      label='학교'
+                      {...register('school')}
+                    />
+                    {errors.school && (
+                      <p className='mt-1 text-sm text-errorTextColor'>
+                        {errors.school.message}
+                      </p>
+                    )}
+                  </div>
+                  <GradeSelector
+                    selectedGrade={selectedGrade}
+                    setSelectedGrade={setSelectedGrade}
+                  />
+                </>
+              )}
+            </div>
+
+            <Button type='submit' variant='active'>
+              변경 내용 저장
+            </Button>
+          </div>
+        </form>
+      </FormProvider>
+
+      <button
+        type='button'
+        className='self-end text-sm font-medium text-captionColor hover:text-textMainColor/70'
+      >
+        탈퇴하기
+      </button>
+    </div>
   );
 };
 
