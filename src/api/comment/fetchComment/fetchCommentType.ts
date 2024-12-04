db144647fa9c12d07982733560a8cca81d425735
doin-N <tlatms777@gmail.com>
2024-12-05 02:24:24 +0900

    ✨ Feat: 댓글 리스트 조회 응답 타입 정의
    #90

--- src/api/comment/fetchComment/fetchCommentType.ts
+++ src/api/comment/fetchComment/fetchCommentType.ts
@@ -1,17 +1,20 @@
-export type FetchCommentRequestParams = {
-  content: string;
-  tags: string[];
-  parent_comment_id?: number;
-};
-
-export type FetchCommentResponseDto = {
+// 댓글 데이터 타입 정의
+export type Comment = {
   comment_id: number;
   post_id: number;
   author_id: number;
   author_nickname: string;
   content: string;
-  created_at: Date;
+  created_at: string;
   tags: string[];
   parent_comment_id: number | null;
-  recomment_count?: number;
+  profile_image: string;
+  recomment_count: number;
+  children: Comment[]; // 대댓글 리스트 (재귀 구조)
+};
+
+// 댓글 리스트 응답 타입 정의
+export type CommentListResponseDto = {
+  comments: Comment[]; // 댓글 리스트
+  total_count: number; // 전체 댓글 개수
 };
