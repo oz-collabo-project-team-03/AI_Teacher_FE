72a11069617f7edcdcb24f8754dfee37f8551c0e
doin-N <tlatms777@gmail.com>
2024-12-05 02:11:10 +0900

    ✨ Feat: 댓글 작성 및 API 연동 구현
    #90

--- src/components/modal/CommentModal.tsx
+++ src/components/modal/CommentModal.tsx
@@ -1,10 +1,13 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useEffect, useRef, useState } from 'react';
 
-import Comment from '../comment/Comment';
+import { Comment } from '@/api/comment/fetchComment/fetchCommentType';
 import CommentHeader from '../comment/CommentHeader';
+import CommentList from '../comment/CommentList';
 import sendIcon from '../../assets/comment/send.svg';
 import student1 from '../../assets/editProfile/student/studentIcon2.png';
+import { useFetchCommentMutation } from '@/api/comment/writeComment/writeComment.hooks';
+import { useFetchCommentQuery } from '@/api/comment/fetchComment/fetchComment.hooks';
 import { useToast } from '@/hooks/useToast';
 
 type commentFormData = {
@@ -12,7 +15,6 @@ type commentFormData = {
 };
 
 const CommentModal = () => {
-  const [comments, setComments] = useState<string[]>([]);
   const [isInputEmpty, setIsInputEmpty] = useState(true);
   const [commentValue, setCommentValue] = useState('');
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
@@ -21,10 +23,47 @@ const CommentModal = () => {
   const {
     register,
     handleSubmit,
-    reset,
     formState: { errors },
     setValue,
   } = useForm<commentFormData>();
+
+  const post_id = 1; //임시 아이디
+
+  const {
+    data: commentData,
+    isLoading,
+    isError,
+    refetch,
+  } = useFetchCommentQuery(post_id);
+
+  const comments: Comment[] = commentData ? commentData.comments : [];
+
+  const { mutate: postComment } = useFetchCommentMutation({
+    onSuccess: () => {
+      showToast('댓글등록성공');
+      refetch();
+      setCommentValue('');
+      setIsInputEmpty(true);
+    },
+    onError: () => {
+      showToast('댓글 등록 중 오류 발생');
+    },
+  });
+
+  const commentOnSubmit: SubmitHandler<commentFormData> = (data) => {
+    const newComment = data.comment?.trim();
+    if (newComment) {
+      postComment({
+        post_id,
+        FetchCommentData: {
+          content: newComment,
+          tags: [],
+        },
+      });
+    } else {
+      showToast('댓글을 입력해주세요.');
+    }
+  };
 
   //textarea 높이제한 변수
   const MAX_ROWS = 7;
@@ -32,27 +71,11 @@ const CommentModal = () => {
   const MAX_HEIGHT = LINE_HEIGHT * MAX_ROWS;
   const MAX_LENGTH = 300;
 
-  // 댓글 제출
-  const commentOnSubmit: SubmitHandler<commentFormData> = (data) => {
-    const newComment = data.comment?.trim();
-    if (newComment) {
-      setComments((prevComments) => [...prevComments, newComment]);
-      reset();
-      setCommentValue('');
-      setIsInputEmpty(true);
-      if (textareaRef.current) {
-        textareaRef.current.style.height = '30px'; // textarea 높이 초기화
-      }
-    } else {
-      showToast('댓글을 입력해주세요.');
-      setIsInputEmpty(true);
-    }
-  };
-
   //textarea 높이조절
   const handleTextareaInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const target = e.target;
     const value = target.value.substring(0, MAX_LENGTH);
+
     target.style.height = '30px';
     target.style.height = `${Math.min(target.scrollHeight, MAX_HEIGHT)}px`;
 
@@ -61,25 +84,29 @@ const CommentModal = () => {
     setIsInputEmpty(value.trim() === '');
   };
 
-  //에러처리
+  //get요청 에러처리
   useEffect(() => {
-    if (errors.comment) {
-      showToast('댓글을 입력해주세요.');
-    }
-  }, [errors.comment, showToast]);
+    if (isError) showToast('댓글을 불러오는 데 실패했습니다.');
+  }, [isError, showToast]);
 
   return (
     <section className='flex h-[70vh] flex-col'>
       <CommentHeader />
       <section className='custom-scrollbar flex-1 overflow-y-auto px-[21px] py-[21px]'>
-        <Comment comments={comments} />
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : isError ? (
+          <p>댓글을 불러오는 데 실패했습니다.</p>
+        ) : (
+          <CommentList comments={comments} refetchComments={refetch} />
+        )}
       </section>
       <form
         onSubmit={handleSubmit(commentOnSubmit)}
         className='bottom-0 flex items-center gap-3 border-t border-chatListHoverColor px-[17px] py-[16px]'
       >
         <img src={student1} alt='comment user' className='h-[35px]' />
-        <div className='relative flex w-full items-center rounded-[15px] bg-commuInputColor p-[14px] placeholder:text-center'>
+        <div className='!important relative flex w-full items-center rounded-[15px] bg-commuInputColor p-[14px] placeholder:text-center'>
           <textarea
             value={commentValue}
             {...register('comment', { required: '댓글을 입력해주세요.' })}
@@ -88,14 +115,13 @@ const CommentModal = () => {
             placeholder='댓글을 입력해주세요'
             aria-label='댓글 입력'
             maxLength={MAX_LENGTH}
-            ref={(el) => {
-              textareaRef.current = el;
-            }}
+            ref={textareaRef}
           />
 
           <button
             type='submit'
             className='absolute right-1 transform rounded p-2 transition-transform'
+            disabled={isInputEmpty}
           >
             <img
               src={sendIcon}
