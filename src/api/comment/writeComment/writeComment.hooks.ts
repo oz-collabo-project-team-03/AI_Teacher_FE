de88b6f89da28d62354838263950f16a5358bff2
doin-N <tlatms777@gmail.com>
2024-12-05 02:24:11 +0900

    ✨ Feat: 댓글 작성 뮤테이션 훅 구현
    #90

---
+++ src/api/comment/writeComment/writeComment.hooks.ts
@@ -0,0 +1,30 @@
+import {
+  FetchCommentRequestParams,
+  FetchCommentResponseDto,
+} from './writeCommentType';
+import {
+  UseMutationOptions,
+  UseMutationResult,
+  useMutation,
+} from '@tanstack/react-query';
+
+import { FetchCommentAPI } from './writeCommentAPI';
+
+export const useFetchCommentMutation = (
+  options?: UseMutationOptions<
+    FetchCommentResponseDto,
+    Error,
+    { post_id: number; FetchCommentData: FetchCommentRequestParams },
+    unknown // 아직 상태나 다른 반환값에 대한 타입이 없으므로 unknown으로 설정
+  >
+): UseMutationResult<
+  FetchCommentResponseDto,
+  Error,
+  { post_id: number; FetchCommentData: FetchCommentRequestParams },
+  unknown
+> => {
+  return useMutation({
+    mutationFn: FetchCommentAPI,
+    ...options,
+  });
+};
