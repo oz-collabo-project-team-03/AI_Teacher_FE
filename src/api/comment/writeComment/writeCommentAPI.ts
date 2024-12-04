19c7645575b731f7424cb5d4ae42cf137c041fda
doin-N <tlatms777@gmail.com>
2024-12-05 02:22:32 +0900

    ✨ Feat: 댓글 작성 API 호출 함수 구현
    #90

---
+++ src/api/comment/writeComment/writeCommentAPI.ts
@@ -0,0 +1,21 @@
+import {
+  FetchCommentRequestParams,
+  FetchCommentResponseDto,
+} from './writeCommentType';
+
+import axiosInstance from '@/api/axiosInstance';
+
+export const FetchCommentAPI = async ({
+  post_id,
+  FetchCommentData,
+}: {
+  post_id: number;
+  FetchCommentData: FetchCommentRequestParams;
+}): Promise<FetchCommentResponseDto> => {
+  console.log('요청 데이터:', FetchCommentData);
+  const response = await axiosInstance.post<FetchCommentResponseDto>(
+    `/comments/write/${post_id}`,
+    FetchCommentData
+  );
+  return response.data;
+};
