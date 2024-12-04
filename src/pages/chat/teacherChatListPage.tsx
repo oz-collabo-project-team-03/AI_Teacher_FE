aed08cf7e3353eeb090dc4ed0e80684d1f604e28
Watnu03 <watnu03@gmail.com>
2024-12-04 15:25:54 +0900

    ✨ Feat: Chat Help List 조회 API 적용

--- src/pages/chat/teacherChatListPage.tsx
+++ src/pages/chat/teacherChatListPage.tsx
@@ -1,44 +1,56 @@
 import ChatItem from '../../components/chat/ChatItem';
 import Header from '../../components/common/Header';
+import { useGetChatHelpQuery } from '../../api/chat/chatHelpList/chatHelpList.hooks';
+import { useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-// 임시데이터>나중에 삭제하기
-const chatList = [
-  {
-    id: '1',
-    nickname: '경원핑',
-    lastMessage: 'Hello there!',
-    lastMessageTime: '오전 9:10',
-    showHelpRequest: true,
-  },
-  {
-    id: '2',
-    nickname: '현주핑',
-    lastMessage: 'How are you?',
-    lastMessageTime: '오후 2:45',
-    showHelpRequest: true,
-  },
-];
 
 const TeacherChatListPage = () => {
   const navigate = useNavigate();
+  const { data: chatList } = useGetChatHelpQuery(1);
 
-  const handleClick = (chatId: string) => {
-    navigate(`/teacher/chats/${chatId}`);
+  const handleClick = (roomId: string) => {
+    navigate(`/teacher/chats/${roomId}`);
   };
+
+  // 최신순 내림차순
+  const recentlyChats = useMemo(() => {
+    if (!chatList) return [];
+
+    return [...chatList].sort((a, b) => {
+      const parseDate = (dateStr: string | null | undefined) => {
+        if (!dateStr) return null;
+
+        const cleanedDateStr = dateStr
+          .replace('시', ':')
+          .replace('분', '')
+          .trim();
+        const date = new Date(cleanedDateStr);
+        return isNaN(date.getTime()) ? null : date;
+      };
+
+      const dateA = parseDate(a.recent_update);
+      const dateB = parseDate(b.recent_update);
+
+      if (!dateA && !dateB) return 0;
+      if (!dateA) return 1;
+      if (!dateB) return -1;
+
+      return dateB.getTime() - dateA.getTime();
+    });
+  }, [chatList]);
 
   return (
     <div className='flex h-full flex-col pt-[72px]'>
       <Header title='최신 채팅' />
-      <div className='scrollbar-hide flex-grow overflow-y-auto'>
-        {chatList.map((chat) => (
+      <div className='custom-scrollbar flex-grow overflow-y-auto'>
+        {recentlyChats?.map((chat) => (
           <ChatItem
-            key={chat.id}
-            roomName={chat.nickname}
-            lastMessage={chat.lastMessage}
-            lastMessageTime={chat.lastMessageTime}
-            showHelpRequest={chat.showHelpRequest}
-            onClick={() => handleClick(chat.id)}
+            key={chat.room_id}
+            roomName={chat.student_nickname}
+            lastMessage={chat.recent_message}
+            lastMessageTime={chat.recent_update}
+            showHelpRequest={chat.help_checked}
+            onClick={() => handleClick(chat.room_id)}
           />
         ))}
       </div>
