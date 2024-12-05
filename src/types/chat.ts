5d65dce6a3d50b4781ad4454c659cf3e832147b1
Watnu03 <watnu03@gmail.com>
2024-12-05 13:33:12 +0900

    🩹 Chore: 타입수정

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,12 +1,15 @@
 export type ChatBubbleProps = {
   message: string;
+  message_type: message_type;
 };
 
 export type ChatMessageProps = {
   message: string;
-  nickname: string;
-  profileImage?: string;
-  isMe: boolean;
-  userType: 'user' | 'ai' | 'teacher' | 'system';
-  isLast?: boolean; // system 만 사용
+  message_type: message_type;
+  nickname: string; 
+  profileImage: string;
+  userType: UserType; 
 };
+
+export type UserType = 'student' | 'teacher' | 'ai' | 'System';
+export type message_type = 'text' | 'image';
