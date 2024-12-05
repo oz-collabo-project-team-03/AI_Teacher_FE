09acf4a5c89854456728728844b748d644401eaa
Watnu03 <watnu03@gmail.com>
2024-12-05 13:30:12 +0900

    🍱 Add: 이미지 프리뷰 아이콘 추가

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,25 +1,27 @@
-import chatDeleteIcon from './chat/chat_delete.svg';
-import chatPlusIcon from './chat/chat_plus.svg';
 import backIcon from './header/back.svg';
-import chatBubbleIcon from './header/chat.svg';
-import iphone from './landing/iphone.svg';
 import bottomArrow from './landing/bottomArrow.svg';
-import modalCloseIcon from './modal/modal_close.svg';
-import editIcon from './myPage/editIcon.svg';
-import logoutIcon from './myPage/logoutIcon.svg';
-import toggleIcon from './myPage/toggleIcon.svg';
 import chatActiveIcon from './navBar/chat_active.svg';
-import homeActiveIcon from './navBar/home_active.svg';
-import plusActiveIcon from './navBar/student/plus_active.svg';
-import userActiveIcon from './navBar/teacher/user_active.svg';
+import chatBubbleIcon from './header/chat.svg';
+import chatDeleteIcon from './chat/chat_delete.svg';
 import chatOutlineIcon from './navBar/chat_outline.svg';
+import chatPlusIcon from './chat/chat_plus.svg';
+import chatPreviewIcon from './chat/chat_preview.svg';
+import editIcon from './myPage/editIcon.svg';
+import homeActiveIcon from './navBar/home_active.svg';
 import homeOutlineIcon from './navBar/home_outline.svg';
+import iphone from './landing/iphone.svg';
+import logoutIcon from './myPage/logoutIcon.svg';
+import modalCloseIcon from './modal/modal_close.svg';
+import plusActiveIcon from './navBar/student/plus_active.svg';
 import plusOutlineIcon from './navBar/student/plus_outline_.svg';
+import toggleIcon from './myPage/toggleIcon.svg';
+import userActiveIcon from './navBar/teacher/user_active.svg';
 import userOutlineIcon from './navBar/teacher/user_outline.svg';
 
 export {
   chatDeleteIcon,
   chatPlusIcon,
+  chatPreviewIcon,
   backIcon,
   chatBubbleIcon,
   iphone,
