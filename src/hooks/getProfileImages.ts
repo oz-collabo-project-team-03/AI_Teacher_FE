b185ab9d8384a68d46de38cb39b4910d821ffb71
Yi-HyeonJu <guswwn1925@gmail.com>
2024-12-06 17:39:33 +0900

    ♻️ 훅 이름 기능에 맞추어 변경

--- src/hooks/getProfileImages.ts
+++ src/hooks/getProfileImages.ts
@@ -1,4 +1,4 @@
-import studentDefaultIcon from '@/assets/editProfile/student/studentDefaultIcon.png';
+import studentIcon1 from '@/assets/editProfile/student/studentIcon1.png';
 import studentIcon2 from '@/assets/editProfile/student/studentIcon2.png';
 import studentIcon3 from '@/assets/editProfile/student/studentIcon3.png';
 import studentIcon4 from '@/assets/editProfile/student/studentIcon4.png';
@@ -10,7 +10,7 @@ import studentIcon9 from '@/assets/editProfile/student/studentIcon9.png';
 import studentIcon10 from '@/assets/editProfile/student/studentIcon10.png';
 import studentIcon11 from '@/assets/editProfile/student/studentIcon11.png';
 import studentIcon12 from '@/assets/editProfile/student/studentIcon12.png';
-import teacherDefaultIcon from '@/assets/editProfile/teacher/teacherDefaultIcon.png';
+import teacherIcon1 from '@/assets/editProfile/teacher/teacherIcon1.png';
 import teacherIcon2 from '@/assets/editProfile/teacher/teacherIcon2.png';
 import teacherIcon3 from '@/assets/editProfile/teacher/teacherIcon3.png';
 
@@ -18,7 +18,7 @@ type UserType = 'student' | 'teacher';
 
 const images = {
   student: [
-    studentDefaultIcon,
+    studentIcon1,
     studentIcon2,
     studentIcon3,
     studentIcon4,
@@ -31,15 +31,24 @@ const images = {
     studentIcon11,
     studentIcon12,
   ],
-  teacher: [teacherDefaultIcon, teacherIcon2, teacherIcon3],
+  teacher: [teacherIcon1, teacherIcon2, teacherIcon3],
 };
 
-export const useProfileImages = (
+export const getProfileImages = (
   userType: UserType,
   currentImageUrl: string,
   onImageSelect: (index: number, imageUrl: string) => void
 ) => {
-  if (currentImageUrl && !images[userType].includes(currentImageUrl)) {
+  // URL에서 파일명 추출 (예: studentIcon3.jpeg -> studentIcon3)
+  const currentFileName = currentImageUrl.split('/').pop()?.split('.')[0];
+
+  // 현재 이미지가 존재하고, 해당 파일명을 포함하는 이미지가 없는 경우
+  const imageExists = images[userType].some((imgPath) => {
+    const importedFileName = imgPath.split('/').pop()?.split('.')[0];
+    return importedFileName === currentFileName;
+  });
+
+  if (currentImageUrl && !imageExists) {
     const defaultIndex = 0;
     onImageSelect(defaultIndex, images[userType][defaultIndex]);
   }
