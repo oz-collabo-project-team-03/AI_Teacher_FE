f73f766bbb32d859abd202a5b41f839ed47667b6
doin-N <tlatms777@gmail.com>
2024-12-07 02:18:41 +0900

    🩹 Chore: post_id 타입을 number에서 string으로 변경
    #90

--- src/api/comment/writeComment/writeComment.hooks.ts
+++ src/api/comment/writeComment/writeComment.hooks.ts
@@ -14,13 +14,13 @@ export const useFetchCommentMutation = (
   options?: UseMutationOptions<
     FetchCommentResponseDto,
     Error,
-    { post_id: number; FetchCommentData: FetchCommentRequestParams },
+    { post_id: string; FetchCommentData: FetchCommentRequestParams },
     unknown // 아직 상태나 다른 반환값에 대한 타입이 없으므로 unknown으로 설정
   >
 ): UseMutationResult<
   FetchCommentResponseDto,
   Error,
-  { post_id: number; FetchCommentData: FetchCommentRequestParams },
+  { post_id: string; FetchCommentData: FetchCommentRequestParams },
   unknown
 > => {
   return useMutation({
