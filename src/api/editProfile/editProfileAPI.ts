d49f1a2e3710ff5ed9bcd47db1850f95f35588a8
Yi-HyeonJu <guswwn1925@gmail.com>
2024-12-07 22:15:20 +0900

    ♻️ 프로필데이터 값 중 필요부분만 서버에 패치
    #148

--- src/api/editProfile/editProfileAPI.ts
+++ src/api/editProfile/editProfileAPI.ts
@@ -7,31 +7,36 @@ import {
 export const patchEditProfileAPI = async (
   profileData: EditProfileRequestParams
 ): Promise<EditProfileResponseDto> => {
-  const endpoint =
-    profileData.role === 'student'
-      ? '/users/profile/student'
-      : '/users/profile/teacher';
-
-  // role,profile_image은 제외
-  const { role, profile_image, ...rest } = profileData;
-
-  // 빈 문자열이 아닌 값만 필터링
-  const filteredData = Object.fromEntries(
-    Object.entries(rest).filter(([_, value]) => value !== '')
-  );
-
-  // 파일명만 추출
-  const profileUpdateData = {
-    ...filteredData,
-    profile_image: profile_image
-      ? profile_image.split('/').pop()?.split('.')[0]
-      : profile_image,
-  };
-
   const response = await axiosInstance.patch<EditProfileResponseDto>(
-    endpoint,
-    profileUpdateData
+    `/users/profile/${profileData.role}`,
+    formatProfileData(profileData)
   );
 
   return response.data;
+};
+
+const formatProfileData = (profileData: EditProfileRequestParams) => {
+  const excludeFields = [
+    'role',
+    'comment_count',
+    'grade',
+    'id',
+    'like_count',
+    'post_count',
+    'posts',
+    'school',
+  ];
+
+  const filteredData = Object.fromEntries(
+    Object.entries(profileData).filter(
+      ([key, value]) => value !== '' && !excludeFields.includes(key)
+    )
+  );
+
+  return {
+    ...filteredData,
+    profile_image: profileData.profile_image
+      ? profileData.profile_image.split('/').pop()?.split('.')[0]
+      : profileData.profile_image,
+  };
 };
