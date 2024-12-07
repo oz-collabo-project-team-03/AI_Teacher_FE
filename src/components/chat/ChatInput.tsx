a03e19398ca23fad48b5ce8faf0212c2707b56ea
Watnu03 <watnu03@gmail.com>
2024-12-07 20:04:41 +0900

    ✨ Feat: 채팅방 help false시, 선생님  input창도 비화설화 옵션 추가

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 
 type ChatInputProps = {
   onSendMessage: (message: string) => void;
+  disabled?: boolean;
 };
 
-const ChatInput = ({ onSendMessage }: ChatInputProps) => {
+const ChatInput = ({ onSendMessage, disabled = false }: ChatInputProps) => {
   const [inputValue, setInputValue] = useState('');
 
   const handleSendOnEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' && !disabled) {
       e.preventDefault();
       if (inputValue.trim() !== '') {
         onSendMessage(inputValue);
@@ -16,8 +17,10 @@ const ChatInput = ({ onSendMessage }: ChatInputProps) => {
       }
     }
   };
-  //글자수 제한
+
+  // 글자수 제한
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (disabled) return;
     const input = e.target.value;
     if (input.length <= 500) {
       setInputValue(input);
@@ -27,11 +30,14 @@ const ChatInput = ({ onSendMessage }: ChatInputProps) => {
   return (
     <input
       type='text'
-      className='w-full rounded-[10px] border-0 bg-commuInputColor py-[18px] pl-[54px] pr-5 outline-none ring-0 ring-inset ring-inputBorderColor placeholder:text-inputBorderColor focus:ring-2 focus:ring-inset focus:ring-inputFocusColor'
-      placeholder='메시지를 입력하세요'
+      className={`w-full rounded-[10px] border-0 bg-commuInputColor py-[18px] pl-[56px] pr-5 outline-none ring-0 ring-inset ring-inputBorderColor placeholder:text-inputBorderColor focus:ring-2 focus:ring-inset focus:ring-inputFocusColor ${
+        disabled ? 'cursor-not-allowed opacity-50' : ''
+      }`}
+      placeholder={disabled ? '비활성화 상태입니다.' : '메시지를 입력하세요'}
       value={inputValue}
       onChange={handleInputChange}
       onKeyDown={handleSendOnEnter}
+      disabled={disabled}
     />
   );
 };
