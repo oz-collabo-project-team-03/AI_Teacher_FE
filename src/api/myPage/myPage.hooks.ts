083b7b604ab2324bb7e1b0ad117fe28370a12a02
deswaq1220 <deswaq789@naver.com>
2024-12-07 19:15:20 +0900

    :bug: 여러번 요청하는 오류 해결

--- src/api/myPage/myPage.hooks.ts
+++ src/api/myPage/myPage.hooks.ts
@@ -9,20 +9,19 @@ export const useProfileGetQuery = (
   userId?: number,
   options?: UseQueryOptions<MyPageResponseDto, Error>
 ) => {
+  const pathname = window.location.pathname;
+
   return useQuery({
-    queryKey: ['profile', userId],
+    queryKey: ['profile', userId, pathname], // 경로도 키에 추가
     queryFn: () => {
-      if (userId) {
-        return getUserProfileAPI(userId);
-      }
-      return getMyProfileAPI();
+      return userId ? getUserProfileAPI(userId) : getMyProfileAPI();
     },
-    enabled: !!cookie.get('accessToken'),
-    retry: 1, // API 오류 시 한 번만 재시도
-    staleTime: 0, // 필요할 때 즉시 새로운 데이터를 가져오기
-    refetchOnWindowFocus: false, // 탭 전환 시 불필요한 리패치를 방지
-    refetchOnReconnect: false, // 네트워크 재연결 시 자동 리패치를 방지합니다
     gcTime: 1000 * 60 * 5, // 캐시된 데이터를 5분간 유지
+    enabled: !!cookie.get('accessToken') && !pathname.includes('member-agree'),
+    staleTime: 1000 * 60 * 5,
+    retry: 1,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
     ...options,
   });
 };
