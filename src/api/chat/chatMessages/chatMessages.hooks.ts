ad13226b0f77516ef10678e4c8f83835207f2e91
Watnu03 <watnu03@gmail.com>
2024-12-10 04:21:20 +0900

    ♻️ Refactor: 무한스크롤을 사용을 위한 코드 수정

--- src/api/chat/chatMessages/chatMessages.hooks.ts
+++ src/api/chat/chatMessages/chatMessages.hooks.ts
@@ -1,21 +1,35 @@
-import { UseQueryOptions, useQuery } from '@tanstack/react-query';
+import {
+  InfiniteData,
+  UseInfiniteQueryOptions,
+  useInfiniteQuery,
+} from '@tanstack/react-query';
 
 import { ChatMessageRequestParams } from './chatMessagesType';
 import { ChatRoomInfoResponseDto } from '@/types/chat';
 import { getChatMessagesAPI } from './chatMessagesAPI';
 
-export const useGetChatMessagesQuery = (
+export const useChatMessagesInfiniteQuery = (
   params: ChatMessageRequestParams,
-  options?: UseQueryOptions<
+  options?: UseInfiniteQueryOptions<
     ChatRoomInfoResponseDto,
     Error,
-    ChatRoomInfoResponseDto,
-    [string, ChatMessageRequestParams]
+    InfiniteData<ChatRoomInfoResponseDto>
   >
 ) => {
-  return useQuery<ChatRoomInfoResponseDto, Error, ChatRoomInfoResponseDto, [string, ChatMessageRequestParams]>({
+  return useInfiniteQuery({
     queryKey: ['chatMessages', params],
-    queryFn: () => getChatMessagesAPI(params),
+    queryFn: async ({ pageParam = 1 }) => {
+      const response = await getChatMessagesAPI({ ...params, page: pageParam as number});
+
+      return response;
+    },
+    getNextPageParam: (lastPage) => {
+      if (!lastPage.pagination.next) {
+        return undefined;
+      }
+      return lastPage.pagination.next;
+    },
+    initialPageParam: 1,
     ...options,
   });
 };
