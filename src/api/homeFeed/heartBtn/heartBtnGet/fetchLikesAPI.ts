9b9bd7dd54a23c86df1a1898f212ce05a6e200de
doin-N <tlatms777@gmail.com>
2024-12-10 02:55:21 +0900

    ✨ Feat: 서버에서 좋아요 상태를 조회하는 API 함수 (fetchLikeStatus) 추가
    #172

---
+++ src/api/homeFeed/heartBtn/heartBtnGet/fetchLikesAPI.ts
@@ -0,0 +1,19 @@
+import { FetchLikeResponseDto } from './fetchLikesType';
+import axiosInstance from '../../../axiosInstance';
+
+export const fetchLikeStatus = async (
+  post_id: string
+): Promise<FetchLikeResponseDto> => {
+  try {
+    const response = await axiosInstance.get<FetchLikeResponseDto>(
+      `/posts/${post_id}/like`
+    );
+    return response.data;
+  } catch (error: any) {
+    console.error(
+      'Failed to fetch like status:',
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+};
