bd25c3f1924345ffbe70d32899d6c1cec403ad40
deswaq1220 <deswaq789@naver.com>
2024-12-11 05:32:34 +0900

    📝 리드미 작성

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <App />
-    <ReactQueryDevtools initialIsOpen={false} />
+    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
   </QueryClientProvider>
 );
